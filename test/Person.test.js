d7ee349bd0bd8e2d27b863c1957b0e5351803590
Shradhdha Parsana <shradhdha03@gmail.com>
2021-12-26 23:34:17 -0500

    changes

--- test/Person.test.js
+++ test/Person.test.js
@@ -1,31 +1,31 @@
-const Person = require('../Person');
+const Person = require('../src/Person');
 const expect = require('expect');
 
-	describe('Person Address', function () {
-		it('should add address for a person', function () {
-			
-			// Arrage
-			let person1 = new Person('John');
+describe('Person Address', function () {
+	it('should add address for a person', function () {
+		
+		// Arrange
+		let person1 = new Person('John');
 
-			// Act
-			person1.addAddress('1111 King St, Toronto');
+		// Act
+		person1.addAddress('1111 King St, Toronto');
 
-			//Assert
-			expect(person1.address).toBe('1111 King St, Toronto');
+		// Assert
+		expect(person1.address).toBe('1111 King St, Toronto');
 
-		});
-
-		it('should give name and address of person', function () {
-
-			// Arrage
-			let person1 = new Person('Nita');
-			person1.addAddress('2222 Queen St, Toronto');
-			
-			// Act
-			const details = person1.getDetails();
-
-			//Assert
-			expect(details).toBe('Name is Nita, Address is: 2222 Queen St, Toronto');
-
-		});
 	});
+
+	it('should give name and address of person', function () {
+
+		// Arrange
+		let person1 = new Person('Nita');
+		person1.addAddress('2222 Queen St, Toronto');
+		
+		// Act
+		const details = person1.getDetails();
+
+		// Assert
+		expect(details).toBe('Name is Nita, Address is: 2222 Queen St, Toronto');
+
+	});
+});
